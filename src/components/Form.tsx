c28122037fc2d0acea91e72fce6b537eace3c4bb
mojzu <mail@mojzu.net>
2017-08-22 23:03:07 +0100

    feat: currency conversion support

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,8 +12,10 @@ export interface IPropsDispatch {
   recalculate: () => void;
   updateCostPrice: (payload: IUpdate) => void;
   updateCostPriceCurrency: (payload: IUpdateCurrency) => void;
+  updateCostPriceCurrencyValue: (payload: IUpdate) => void;
   updateSalePrice: (payload: IUpdate) => void;
   updateSalePriceCurrency: (payload: IUpdateCurrency) => void;
+  updateSalePriceCurrencyValue: (payload: IUpdate) => void;
   updateMargin: (payload: IUpdate) => void;
   updateMarkup: (payload: IUpdate) => void;
   ratesRequest: () => void;
@@ -25,9 +27,10 @@ export class Form extends React.Component<IProps> {
 
   public static navigationOptions = {
     title: "Margin Calculator",
-    headerStyle: {
-      backgroundColor: "skyblue",
-    },
+    // TODO: Style improvements.
+    // headerStyle: {
+    //   backgroundColor: "skyblue",
+    // },
   };
 
   public constructor(props: IProps) {
@@ -46,11 +49,17 @@ export class Form extends React.Component<IProps> {
     const onCostPriceCurrencyChanged = this.onCostPriceCurrencyChanged.bind(this);
     const costPriceCurrency = this.props.marginCalculator.displayCostPriceCurrency;
 
+    const onCostPriceCurrencyValueChanged = this.onCostPriceCurrencyValueChanged.bind(this);
+    const costPriceCurrencyValue = this.props.marginCalculator.displayCostPriceCurrencyValue;
+
     const onSalePriceChanged = this.onSalePriceChanged.bind(this);
     const salePrice = this.props.marginCalculator.displaySalePrice;
 
     const onSalePriceCurrencyChanged = this.onSalePriceCurrencyChanged.bind(this);
     const salePriceCurrency = this.props.marginCalculator.displaySalePriceCurrency;
+
+    const onSalePriceCurrencyValueChanged = this.onSalePriceCurrencyValueChanged.bind(this);
+    const salePriceCurrencyValue = this.props.marginCalculator.displaySalePriceCurrencyValue;
 
     const onMarginChanged = this.onMarginChanged.bind(this);
     const margin = this.props.marginCalculator.displayMargin;
@@ -90,6 +99,13 @@ export class Form extends React.Component<IProps> {
           >
             {currencyItems}
           </Picker>
+          <TextInput
+            style={styles.textInput}
+            keyboardType="numeric"
+            onChangeText={onCostPriceCurrencyValueChanged}
+            onEndEditing={onRecalculate}
+            value={costPriceCurrencyValue}
+          />
         </View>
 
         <View style={styles.inputContainer}>
@@ -115,6 +131,13 @@ export class Form extends React.Component<IProps> {
           >
             {currencyItems}
           </Picker>
+          <TextInput
+            style={styles.textInput}
+            keyboardType="numeric"
+            onChangeText={onSalePriceCurrencyValueChanged}
+            onEndEditing={onRecalculate}
+            value={salePriceCurrencyValue}
+          />
         </View>
 
         <View style={styles.inputContainer}>
@@ -161,8 +184,12 @@ export class Form extends React.Component<IProps> {
   }
 
   protected onCostPriceCurrencyChanged(label: string): void {
-    const value = (label === this.props.currencyRates.base) ? 1 : this.props.currencyRates.rates[label];
+    const value = ((label === this.props.currencyRates.base) ? 1 : this.props.currencyRates.rates[label]).toFixed(4);
     this.props.updateCostPriceCurrency({ label, value });
+  }
+
+  protected onCostPriceCurrencyValueChanged(value: string): void {
+    this.props.updateCostPriceCurrencyValue({ value });
   }
 
   protected onSalePriceChanged(value: string): void {
@@ -170,8 +197,12 @@ export class Form extends React.Component<IProps> {
   }
 
   protected onSalePriceCurrencyChanged(label: string): void {
-    const value = (label === this.props.currencyRates.base) ? 1 : this.props.currencyRates.rates[label];
+    const value = ((label === this.props.currencyRates.base) ? 1 : this.props.currencyRates.rates[label]).toFixed(4);
     this.props.updateSalePriceCurrency({ label, value });
+  }
+
+  protected onSalePriceCurrencyValueChanged(value: string): void {
+    this.props.updateSalePriceCurrencyValue({ value });
   }
 
   protected onMarginChanged(value: string): void {
--- src/store.ts
+++ src/store.ts
@@ -10,12 +10,16 @@ import { navigationReducer } from "./navigation";
 export interface IMarginCalculator {
   displayCostPrice: string;
   displayCostPriceCurrency: string;
+  displayCostPriceCurrencyValue: string;
   displaySalePrice: string;
   displaySalePriceCurrency: string;
+  displaySalePriceCurrencyValue: string;
   displayMargin: string;
   displayMarkup: string;
   costPrice: number;
+  costPriceCurrency: number;
   salePrice: number;
+  salePriceCurrency: number;
   margin: number;
   markup: number;
   lastUpdate: string;
@@ -24,12 +28,16 @@ export interface IMarginCalculator {
 const marginCalculatorDefaultState: IMarginCalculator = {
   displayCostPrice: "",
   displayCostPriceCurrency: "GBP",
+  displayCostPriceCurrencyValue: "1.0000",
   displaySalePrice: "",
   displaySalePriceCurrency: "GBP",
+  displaySalePriceCurrencyValue: "1.0000",
   displayMargin: "",
   displayMarkup: "",
   costPrice: 0,
+  costPriceCurrency: 1,
   salePrice: 0,
+  salePriceCurrency: 1,
   margin: 0,
   markup: 0,
   lastUpdate: "",
@@ -47,11 +55,17 @@ export type UPDATE_COST_PRICE = typeof UPDATE_COST_PRICE;
 export const UPDATE_COST_PRICE_CURRENCY = "MarginCalculator/UpdateCostPriceCurrency";
 export type UPDATE_COST_PRICE_CURRENCY = typeof UPDATE_COST_PRICE_CURRENCY;
 
+export const UPDATE_COST_PRICE_CURRENCY_VALUE = "MarginCalculator/UpdateCostPriceCurrencyValue";
+export type UPDATE_COST_PRICE_CURRENCY_VALUE = typeof UPDATE_COST_PRICE_CURRENCY_VALUE;
+
 export const UPDATE_SALE_PRICE = "MarginCalculator/UpdateSalePrice";
 export type UPDATE_SALE_PRICE = typeof UPDATE_SALE_PRICE;
 
 export const UPDATE_SALE_PRICE_CURRENCY = "MarginCalculator/UpdateSalePriceCurrency";
 export type UPDATE_SALE_PRICE_CURRENCY = typeof UPDATE_SALE_PRICE_CURRENCY;
+
+export const UPDATE_SALE_PRICE_CURRENCY_VALUE = "MarginCalculator/UpdateSalePriceCurrencyValue";
+export type UPDATE_SALE_PRICE_CURRENCY_VALUE = typeof UPDATE_SALE_PRICE_CURRENCY_VALUE;
 
 export const UPDATE_MARGIN = "MarginCalculator/UpdateMargin";
 export type UPDATE_MARGIN = typeof UPDATE_MARGIN;
@@ -65,117 +79,178 @@ export interface IUpdate {
 
 export interface IUpdateCurrency {
   label: string;
-  value: number;
+  value: string;
 }
 
 export const reset = createAction<void>(RESET);
 export const recalculate = createAction<void>(RECALCULATE);
 export const updateCostPrice = createAction<IUpdate>(UPDATE_COST_PRICE);
 export const updateCostPriceCurrency = createAction<IUpdateCurrency>(UPDATE_COST_PRICE_CURRENCY);
+export const updateCostPriceCurrencyValue = createAction<IUpdate>(UPDATE_COST_PRICE_CURRENCY_VALUE);
 export const updateSalePrice = createAction<IUpdate>(UPDATE_SALE_PRICE);
 export const updateSalePriceCurrency = createAction<IUpdateCurrency>(UPDATE_SALE_PRICE_CURRENCY);
+export const updateSalePriceCurrencyValue = createAction<IUpdate>(UPDATE_SALE_PRICE_CURRENCY_VALUE);
 export const updateMargin = createAction<IUpdate>(UPDATE_MARGIN);
 export const updateMarkup = createAction<IUpdate>(UPDATE_MARKUP);
+
+function recalculateMargin(state: IMarginCalculator): void {
+  state.margin = ((state.salePrice - state.costPrice) / state.salePrice) * 100;
+  state.displayMargin = state.margin.toFixed(2);
+}
+
+function recalculateMarkup(state: IMarginCalculator): void {
+  state.markup = ((state.salePrice / state.costPrice) * 100) - 100;
+  state.displayMarkup = state.markup.toFixed(2);
+}
+
+function recalculateSalePriceFromMargin(state: IMarginCalculator): void {
+  state.salePrice = state.costPrice / (1 - (state.margin / 100));
+  recalculateSalePriceCurrency(state);
+}
+
+function recalculateSalePriceFromMarkup(state: IMarginCalculator): void {
+  state.salePrice = (state.costPrice * (state.markup / 100)) + state.costPrice;
+  recalculateSalePriceCurrency(state);
+}
+
+function recalculateCostPriceFromMargin(state: IMarginCalculator): void {
+  state.costPrice = state.salePrice - ((state.margin / 100) * state.salePrice);
+  recalculateCostPriceCurrency(state);
+}
+
+function recalculateCostPriceFromMarkup(state: IMarginCalculator): void {
+  state.costPrice = state.salePrice / ((state.markup + 100) / 100);
+  recalculateCostPriceCurrency(state);
+}
+
+function recalculateSalePriceCurrency(state: IMarginCalculator): void {
+  if (!!state.salePrice) {
+    const salePriceInCurrency = state.salePrice * (state.salePriceCurrency / state.costPriceCurrency);
+    state.displaySalePrice = salePriceInCurrency.toFixed(2);
+  }
+}
+
+function recalculateCostPriceCurrency(state: IMarginCalculator): void {
+  if (!!state.costPrice) {
+    const costPriceInCurrency = state.costPrice * (state.costPriceCurrency / state.salePriceCurrency);
+    state.displayCostPrice = costPriceInCurrency.toFixed(2);
+  }
+}
+
+function recalculateState(state: IMarginCalculator): IMarginCalculator {
+  const newState = {
+    ...state,
+    costPrice: parseFloat(state.displayCostPrice),
+    costPriceCurrency: parseFloat(state.displayCostPriceCurrencyValue),
+    salePrice: parseFloat(state.displaySalePrice),
+    salePriceCurrency: parseFloat(state.displaySalePriceCurrencyValue),
+    margin: parseFloat(state.displayMargin),
+    markup: parseFloat(state.displayMarkup),
+  };
+
+  if (!!newState.costPrice && (newState.lastUpdate === UPDATE_COST_PRICE)) {
+    if (!!newState.salePrice) {
+
+      recalculateMargin(newState);
+      recalculateMarkup(newState);
+
+    } else if (!!newState.margin) {
+
+      recalculateSalePriceFromMargin(newState);
+      recalculateMarkup(newState);
+
+    } else if (!!newState.markup) {
+
+      recalculateSalePriceFromMarkup(newState);
+      recalculateMargin(newState);
+
+    }
+  } else if (!!newState.salePrice && (newState.lastUpdate === UPDATE_SALE_PRICE)) {
+    if (!!newState.costPrice) {
+
+      recalculateMargin(newState);
+      recalculateMarkup(newState);
+
+    } else if (!!newState.margin) {
+
+      recalculateCostPriceFromMargin(newState);
+      recalculateMarkup(newState);
+
+    } else if (!!newState.markup) {
+
+      recalculateCostPriceFromMarkup(newState);
+      recalculateMargin(newState);
+
+    }
+  } else if (!!newState.margin && (newState.lastUpdate === UPDATE_MARGIN)) {
+    if (!!newState.costPrice) {
+
+      recalculateSalePriceFromMargin(newState);
+      recalculateMarkup(newState);
+
+    } else if (!!newState.salePrice) {
+
+      recalculateCostPriceFromMargin(newState);
+      recalculateMarkup(newState);
+
+    }
+  } else if (!!newState.markup && (newState.lastUpdate === UPDATE_MARKUP)) {
+    if (!!newState.costPrice) {
+
+      recalculateSalePriceFromMarkup(newState);
+      recalculateMargin(newState);
+
+    } else if (!!newState.salePrice) {
+
+      recalculateCostPriceFromMarkup(newState);
+      recalculateMargin(newState);
+
+    }
+  } else if (!!newState.costPriceCurrency && (newState.lastUpdate === UPDATE_COST_PRICE_CURRENCY)) {
+    if (!!newState.salePrice) {
+      if (!!newState.margin) {
+
+        recalculateCostPriceFromMargin(newState);
+        recalculateMarkup(newState);
+
+      } else if (!!newState.markup) {
+
+        recalculateCostPriceFromMarkup(newState);
+        recalculateMargin(newState);
+
+      }
+    }
+
+    recalculateCostPriceCurrency(newState);
+
+  } else if (!!newState.salePriceCurrency && (newState.lastUpdate === UPDATE_SALE_PRICE_CURRENCY)) {
+    if (!!newState.costPrice) {
+      if (!!newState.margin) {
+
+        recalculateSalePriceFromMargin(newState);
+        recalculateMarkup(newState);
+
+      } else if (!!newState.markup) {
+
+        recalculateSalePriceFromMarkup(newState);
+        recalculateMargin(newState);
+
+      }
+    }
+
+    recalculateSalePriceCurrency(newState);
+
+  }
+
+  return newState;
+}
 
 export const marginCalculatorReducer = handleActions<IMarginCalculator, any>({
   [RESET]: (state, action) => {
     return marginCalculatorDefaultState;
   },
   [RECALCULATE]: (state, action) => {
-
-    const newState = {
-      ...state,
-      costPrice: parseFloat(state.displayCostPrice),
-      salePrice: parseFloat(state.displaySalePrice),
-      margin: parseFloat(state.displayMargin),
-      markup: parseFloat(state.displayMarkup),
-    };
-
-    // Start calculation.
-
-    if (!!newState.costPrice && (newState.lastUpdate === UPDATE_COST_PRICE)) {
-      if (!!newState.salePrice) {
-
-        newState.margin = ((newState.salePrice - newState.costPrice) / newState.salePrice) * 100;
-        newState.displayMargin = newState.margin.toFixed(2);
-        newState.markup = ((newState.salePrice / newState.costPrice) * 100) - 100;
-        newState.displayMarkup = newState.markup.toFixed(2);
-
-      } else if (!!newState.margin) {
-
-        newState.salePrice = newState.costPrice / (1 - (newState.margin / 100));
-        newState.displaySalePrice = newState.salePrice.toFixed(2);
-        newState.markup = ((newState.salePrice / newState.costPrice) * 100) - 100;
-        newState.displayMarkup = newState.markup.toFixed(2);
-
-      } else if (!!newState.markup) {
-
-        newState.salePrice = (newState.costPrice * (newState.markup / 100)) + newState.costPrice;
-        newState.displaySalePrice = newState.salePrice.toFixed(2);
-        newState.margin = ((newState.salePrice - newState.costPrice) / newState.salePrice) * 100;
-        newState.displayMargin = newState.margin.toFixed(2);
-
-      }
-    } else if (!!newState.salePrice && (newState.lastUpdate === UPDATE_SALE_PRICE)) {
-      if (!!newState.costPrice) {
-
-        newState.margin = ((newState.salePrice - newState.costPrice) / newState.salePrice) * 100;
-        newState.displayMargin = newState.margin.toFixed(2);
-        newState.markup = ((newState.salePrice / newState.costPrice) * 100) - 100;
-        newState.displayMarkup = newState.markup.toFixed(2);
-
-      } else if (!!newState.margin) {
-
-        newState.costPrice = newState.salePrice - ((newState.margin / 100) * newState.salePrice);
-        newState.displayCostPrice = newState.costPrice.toFixed(2);
-        newState.markup = ((newState.salePrice / newState.costPrice) * 100) - 100;
-        newState.displayMarkup = newState.markup.toFixed(2);
-
-      } else if (!!newState.markup) {
-
-        newState.costPrice = newState.salePrice / ((newState.markup + 100) / 100);
-        newState.displayCostPrice = newState.costPrice.toFixed(2);
-        newState.margin = ((newState.salePrice - newState.costPrice) / newState.salePrice) * 100;
-        newState.displayMargin = newState.margin.toFixed(2);
-
-      }
-    } else if (!!newState.margin && (newState.lastUpdate === UPDATE_MARGIN)) {
-      if (!!newState.costPrice) {
-
-        newState.salePrice = newState.costPrice / (1 - (newState.margin / 100));
-        newState.displaySalePrice = newState.salePrice.toFixed(2);
-        newState.markup = ((newState.salePrice / newState.costPrice) * 100) - 100;
-        newState.displayMarkup = newState.markup.toFixed(2);
-
-      } else if (!!newState.salePrice) {
-
-        newState.costPrice = newState.salePrice - ((newState.margin / 100) * newState.salePrice);
-        newState.displayCostPrice = newState.costPrice.toFixed(2);
-        newState.markup = ((newState.salePrice / newState.costPrice) * 100) - 100;
-        newState.displayMarkup = newState.markup.toFixed(2);
-
-      }
-    } else if (!!newState.markup && (newState.lastUpdate === UPDATE_MARKUP)) {
-      if (!!newState.costPrice) {
-
-        newState.salePrice = (newState.costPrice * (newState.markup / 100)) + newState.costPrice;
-        newState.displaySalePrice = newState.salePrice.toFixed(2);
-        newState.margin = ((newState.salePrice - newState.costPrice) / newState.salePrice) * 100;
-        newState.displayMargin = newState.margin.toFixed(2);
-
-      } else if (!!newState.salePrice) {
-
-        newState.costPrice = newState.salePrice / ((newState.markup + 100) / 100);
-        newState.displayCostPrice = newState.costPrice.toFixed(2);
-        newState.margin = ((newState.salePrice - newState.costPrice) / newState.salePrice) * 100;
-        newState.displayMargin = newState.margin.toFixed(2);
-
-      }
-    }
-
-    // End calculation.
-
-    return newState;
+    return recalculateState(state);
   },
   [UPDATE_COST_PRICE]: (state, action) => {
     if (!!action.payload) {
@@ -189,9 +264,21 @@ export const marginCalculatorReducer = handleActions<IMarginCalculator, any>({
   },
   [UPDATE_COST_PRICE_CURRENCY]: (state, action) => {
     if (!!action.payload) {
-      return {
+      const newState = {
         ...state,
         displayCostPriceCurrency: action.payload.label,
+        displayCostPriceCurrencyValue: action.payload.value,
+        lastUpdate: UPDATE_COST_PRICE_CURRENCY,
+      };
+      return recalculateState(newState);
+    }
+    return state;
+  },
+  [UPDATE_COST_PRICE_CURRENCY_VALUE]: (state, action) => {
+    if (!!action.payload) {
+      return {
+        ...state,
+        displayCostPriceCurrencyValue: action.payload.value,
         lastUpdate: UPDATE_COST_PRICE_CURRENCY,
       };
     }
@@ -209,9 +296,21 @@ export const marginCalculatorReducer = handleActions<IMarginCalculator, any>({
   },
   [UPDATE_SALE_PRICE_CURRENCY]: (state, action) => {
     if (!!action.payload) {
-      return {
+      const newState = {
         ...state,
         displaySalePriceCurrency: action.payload.label,
+        displaySalePriceCurrencyValue: action.payload.value,
+        lastUpdate: UPDATE_SALE_PRICE_CURRENCY,
+      };
+      return recalculateState(newState);
+    }
+    return state;
+  },
+  [UPDATE_SALE_PRICE_CURRENCY_VALUE]: (state, action) => {
+    if (!!action.payload) {
+      return {
+        ...state,
+        displaySalePriceCurrencyValue: action.payload.value,
         lastUpdate: UPDATE_SALE_PRICE_CURRENCY,
       };
     }
@@ -253,7 +352,7 @@ export interface ICurrencyRates {
 
 const currencyRatesDefaultState: ICurrencyRates = {
   base: "GBP",
-  date: "2017-08-21",
+  date: "2017-08-22",
   rates: {},
 };
 
