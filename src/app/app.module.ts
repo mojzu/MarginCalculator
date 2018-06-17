79da8761e1d7c616fea65aa87552885ba3dc9caf
mojzu <mail@mojzu.net>
2018-06-18 00:11:03 +0100

    chore: add resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
+import { IonicStorageModule } from "@ionic/storage";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { MarginCalculatorPage } from "../pages/margin-calculator/margin-calculator";
 import { MyApp } from "./app.component";
 
 @NgModule({
   declarations: [MyApp, MarginCalculatorPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), IonicStorageModule.forRoot()],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, MarginCalculatorPage],
   providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }]
