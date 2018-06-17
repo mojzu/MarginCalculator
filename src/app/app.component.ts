46ec1322577551a51ae1b775034e8d498e8b1021
mojzu <mail@mojzu.net>
2018-06-17 23:48:14 +0100

    chore: icons testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       statusBar.styleLightContent();
-      statusBar.backgroundColorByHexString("#4040bf");
+      statusBar.backgroundColorByHexString("#333399");
       splashScreen.hide();
     });
   }
