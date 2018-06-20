bc80ee57450543df952efe0f6bcf8ab47e4301fe
mojzu <mail@mojzu.net>
2018-06-20 01:07:13 +0100

    feat: add exchange rates caching

--- src/providers/store/store.ts
+++ src/providers/store/store.ts
@@ -4,6 +4,7 @@ import { Storage } from "@ionic/storage";
 import { Actions, Effect, ofType } from "@ngrx/effects";
 import { Action, Store } from "@ngrx/store";
 import { isFloat } from "container.ts/lib/validate";
+import { ToastController } from "ionic-angular";
 import { get, isArray, isString } from "lodash";
 import { Observable } from "rxjs";
 import { catchError, map, mergeMap } from "rxjs/operators";
@@ -11,12 +12,17 @@ import { parseString } from "xml2js";
 import { IState } from "../../store";
 import * as Currencies from "../../store/currencies";
 
+export interface ICache {
+  time: string;
+  currenciesRates: Currencies.ICurrenciesRates;
+}
+
 @Injectable()
 export class StoreProvider {
   @Effect()
   public effectsInit$: Observable<Action> = this.actions$.pipe(
     ofType("@ngrx/effects/init"),
-    map(() => new Currencies.GetRequest())
+    mergeMap(() => this.effectsInit())
   );
 
   @Effect()
@@ -25,9 +31,22 @@ export class StoreProvider {
     mergeMap(() => this.currenciesGetRequest())
   );
 
+  @Effect()
+  public currenciesGetResponse$: Observable<Action> = this.actions$.pipe(
+    ofType(Currencies.EActions.GetResponse),
+    mergeMap((action) => this.currenciesGetResponse(action as Currencies.GetResponse))
+  );
+
+  @Effect()
+  public currenciesGetError$: Observable<Action> = this.actions$.pipe(
+    ofType(Currencies.EActions.GetError),
+    mergeMap((action) => this.currenciesGetError(action as Currencies.GetError))
+  );
+
   public constructor(
     protected readonly http: HttpClient,
     protected readonly storage: Storage,
+    protected readonly toastController: ToastController,
     protected readonly store: Store<IState>,
     protected readonly actions$: Actions
   ) {}
@@ -38,6 +57,24 @@ export class StoreProvider {
 
   public select<S>(mapFn: (state: IState) => S): Observable<S> {
     return this.store.select<S>(mapFn);
+  }
+
+  protected effectsInit(): Observable<Action> {
+    return this.loadCache().pipe(
+      map((cache) => {
+        if (cache != null) {
+          const today = new Date().toISOString().slice(0, 10);
+          if (cache.time < today) {
+            // Get exchange rates if cache expired.
+            return new Currencies.GetRequest();
+          }
+          // Use cached exchange rates.
+          return new Currencies.GetResponse(cache);
+        }
+        // Get exchange rates, not cached.
+        return new Currencies.GetRequest();
+      })
+    );
   }
 
   protected currenciesGetRequest(): Observable<Action> {
@@ -77,6 +114,28 @@ export class StoreProvider {
     );
   }
 
+  protected currenciesGetResponse(action: Currencies.GetResponse): Observable<Action> {
+    return this.saveCache(action.payload).pipe(
+      map((cache) => {
+        this.presentToast(`Exchange rates from ${cache.time}.`);
+        return { type: "Null" };
+      })
+    );
+  }
+
+  protected currenciesGetError(action: Currencies.GetError): Observable<Action> {
+    this.presentToast(`Error getting exchange rates.`);
+    return Observable.of({ type: "Null" });
+  }
+
+  protected loadCache(): Observable<ICache | undefined> {
+    return Observable.fromPromise(this.storage.get("cache"));
+  }
+
+  protected saveCache(cache: ICache): Observable<ICache> {
+    return Observable.fromPromise(this.storage.set("cache", cache)).pipe(map(() => cache));
+  }
+
   protected parseXml(xml: string): Promise<object> {
     return new Promise((resolve, reject) => {
       parseString(xml, (error: any, result: object) => {
@@ -87,5 +146,14 @@ export class StoreProvider {
         }
       });
     });
+  }
+
+  protected presentToast(message: string): void {
+    const toast = this.toastController.create({
+      message,
+      duration: 3000,
+      position: "bottom"
+    });
+    toast.present();
   }
 }
