85a8b715d1bcd98f5c8fa99cc6bf717942f0b465
mojzu <mail@mojzu.net>
2018-10-01 21:56:03 +0100

    feat: cost and sale price currencies are cached after selection

--- src/providers/store/store.ts
+++ src/providers/store/store.ts
@@ -7,22 +7,36 @@ import { isFloat } from "container.ts/lib/validate";
 import { ToastController } from "ionic-angular";
 import { get, isArray, isString } from "lodash";
 import { Observable } from "rxjs";
-import { catchError, map, mergeMap } from "rxjs/operators";
+import { catchError, filter, map, mergeMap } from "rxjs/operators";
 import { parseString } from "xml2js";
 import { IState } from "../../store";
+import * as Calculator from "../../store/calculator";
 import * as Currencies from "../../store/currencies";
 
 export interface ICache {
-  time: string;
-  currenciesRates: Currencies.ICurrenciesRates;
+  getResponse?: { time: string; currenciesRates: Currencies.ICurrenciesRates };
+  costPriceCurrency?: string;
+  salePriceCurrency?: string;
 }
 
 @Injectable()
 export class StoreProvider {
   @Effect()
-  public effectsInit$: Observable<Action> = this.actions$.pipe(
+  public cacheGetResponseInit$: Observable<Action> = this.actions$.pipe(
     ofType("@ngrx/effects/init"),
-    mergeMap(() => this.effectsInit())
+    mergeMap(() => this.cacheGetResponse())
+  );
+
+  @Effect()
+  public cacheCostPriceCurrencyInit$: Observable<Action> = this.actions$.pipe(
+    ofType(Currencies.EActions.GetResponse),
+    mergeMap((action) => this.cacheCostPriceCurrency(action as Currencies.GetResponse))
+  );
+
+  @Effect()
+  public cacheSalePriceCurrencyInit$: Observable<Action> = this.actions$.pipe(
+    ofType(Currencies.EActions.GetResponse),
+    mergeMap((action) => this.cacheSalePriceCurrency(action as Currencies.GetResponse))
   );
 
   @Effect()
@@ -43,6 +57,18 @@ export class StoreProvider {
     mergeMap((action) => this.currenciesGetError(action as Currencies.GetError))
   );
 
+  @Effect()
+  public calculatorSetCostPriceCurrency$: Observable<Action> = this.actions$.pipe(
+    ofType(Calculator.EActions.UpdateCostPriceCurrency),
+    mergeMap((action) => this.calculatorUpdateCostPriceCurrency(action as Calculator.UpdateCostPriceCurrency))
+  );
+
+  @Effect()
+  public calculatorSetSalePriceCurrency$: Observable<Action> = this.actions$.pipe(
+    ofType(Calculator.EActions.UpdateSalePriceCurrency),
+    mergeMap((action) => this.calculatorUpdateSalePriceCurrency(action as Calculator.UpdateSalePriceCurrency))
+  );
+
   public constructor(
     protected readonly http: HttpClient,
     protected readonly storage: Storage,
@@ -59,20 +85,40 @@ export class StoreProvider {
     return this.store.select<S>(mapFn);
   }
 
-  protected effectsInit(): Observable<Action> {
+  protected cacheGetResponse(): Observable<Action> {
     return this.loadCache().pipe(
       map((cache) => {
-        if (cache != null) {
+        if (cache != null && cache.getResponse != null) {
           const today = new Date().toISOString().slice(0, 10);
-          if (cache.time < today) {
+          if (cache.getResponse.time < today) {
             // Get exchange rates if cache expired.
             return new Currencies.GetRequest();
           }
           // Use cached exchange rates.
-          return new Currencies.GetResponse(cache);
+          return new Currencies.GetResponse(cache.getResponse);
         }
         // Get exchange rates, not cached.
         return new Currencies.GetRequest();
+      })
+    );
+  }
+
+  protected cacheCostPriceCurrency(action: Currencies.GetResponse): Observable<Action> {
+    return this.loadCache().pipe(
+      filter((cache) => cache != null && cache.costPriceCurrency != null),
+      map((cache) => {
+        const value = get(action.payload.currenciesRates, cache.costPriceCurrency, 1).toFixed(4);
+        return new Calculator.UpdateCostPriceCurrency({ key: cache.costPriceCurrency, value });
+      })
+    );
+  }
+
+  protected cacheSalePriceCurrency(action: Currencies.GetResponse): Observable<Action> {
+    return this.loadCache().pipe(
+      filter((cache) => cache != null && cache.salePriceCurrency != null),
+      map((cache) => {
+        const value = get(action.payload.currenciesRates, cache.salePriceCurrency, 1).toFixed(4);
+        return new Calculator.UpdateSalePriceCurrency({ key: cache.salePriceCurrency, value });
       })
     );
   }
@@ -117,9 +163,9 @@ export class StoreProvider {
   }
 
   protected currenciesGetResponse(action: Currencies.GetResponse): Observable<Action> {
-    return this.saveCache(action.payload).pipe(
+    return this.saveCache({ getResponse: action.payload }).pipe(
       map((cache) => {
-        this.presentToast(`Exchange rates from ${cache.time}.`);
+        this.presentToast(`Exchange rates from ${cache.getResponse.time}.`);
         return { type: "Null" };
       })
     );
@@ -130,12 +176,31 @@ export class StoreProvider {
     return Observable.of({ type: "Null" });
   }
 
+  protected calculatorUpdateCostPriceCurrency(action: Calculator.UpdateCostPriceCurrency): Observable<Action> {
+    return this.saveCache({ costPriceCurrency: action.payload.key }).pipe(
+      map((cache) => ({ type: "Null" }))
+    );
+  }
+
+  protected calculatorUpdateSalePriceCurrency(action: Calculator.UpdateSalePriceCurrency): Observable<Action> {
+    return this.saveCache({ salePriceCurrency: action.payload.key }).pipe(
+      map((cache) => ({ type: "Null" }))
+    );
+  }
+
   protected loadCache(): Observable<ICache | undefined> {
     return Observable.fromPromise(this.storage.get("cache"));
   }
 
-  protected saveCache(cache: ICache): Observable<ICache> {
-    return Observable.fromPromise(this.storage.set("cache", cache)).pipe(map(() => cache));
+  protected saveCache(updateCache: Partial<ICache>): Observable<ICache> {
+    return this.loadCache().pipe(
+      mergeMap((currentCache) => {
+        const cache = { ...currentCache, ...updateCache };
+        return Observable.fromPromise(this.storage.set("cache", cache));
+      }),
+      mergeMap(() => this.loadCache()),
+      map((cache) => cache)
+    );
   }
 
   protected parseXml(xml: string): Promise<object> {
